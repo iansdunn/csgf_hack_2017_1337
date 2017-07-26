f959c12516980a380c876d19b423b4197452a5e4
Ian Dunn <iansdunn@gmail.com>
2017-07-26 13:53:47 -0400

    Bug fix: Mandelbrot

--- src/Mandelbrot.cpp
+++ src/Mandelbrot.cpp
@@ -1,21 +1,21 @@
 // Mandelbrot.cpp
 // Created by Laura Watkins.
 
-include "Mandelbrot.h"
+#include "Mandelbrot.h"
 
 int Mandelbrot::calculate_mandelbrot(double x0, double y0) {
     int iter = 0;
     double rad_z = 0.0;
     double x = 0.0;
     double y = 0.0;
-    while ( rad_z < rad_max**2 && iter < iter_max ) {
-        xtemp = x*x - y*y + x0 ;
+    while ( rad_z < pow(rad_max_, 2) && iter < iter_max_ ) {
+        double xtemp = x * x - y * y + x0 ;
         y = 2*x*y + y0 ;
         x = xtemp ;
         rad_z = x*x + y*y ;
         iter = iter + 1 ;
     }
-    if ( iter < iter_max) {
+    if ( iter < iter_max_) {
         return 0;
     }
     else {
@@ -29,8 +29,8 @@ double Mandelbrot::color(double distance, Grid* grid)
 
     if( distance < half_pixel_size )
     {
-        return pow(distance / half_pixel_size, 1. / 3.) * 255.
+        return pow(distance / half_pixel_size, 1. / 3.) * 255.;
     }
     else
-        return 255.
+        return 255.;
 }
