c96b0e5013f4773062b7de965c15a4979ff31c6c
Ian Dunn <iansdunn@gmail.com>
2017-07-26 13:53:32 -0400

    Bug fixes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,27 @@
 #include "mpi.h"
 #include <assert.h>
 #include <limits>
-#include "grind.h" 
+#include "Grid.h" 
+#include "Mandelbrot.h"
+
+using namespace std;
 
 class MyFunctor { 
 public:
-  MyFunctor(Kokkos::View<double*> a) : a{a} {}
+  MyFunctor(Kokkos::View<double*> a, Grid* grid, Mandelbrot* mb) : a_(a), grid_(grid), mb_(mb) {}
   
   KOKKOS_INLINE_FUNCTION 
   void operator() (int i) const {  
-    std::pair<double, double> xy = grid.calculate_xy(i);                      
-    a(i) = Mandelbrot.calculate_mandelbrot(xy.first, xy.second);
+    pair<double, double> xy = grid_->calculate_xy(i);                      
+    a_(i) = mb_->calculate_mandelbrot(xy.first, xy.second);
   }
-};
 
 private:
-  Kokkos::View<double*> a;
+  Kokkos::View<double*> a_;
+
+  Grid* grid_;
+
+  Mandelbrot* mb_;
 };
 
 int main(int argc, char **argv) {
@@ -35,27 +41,30 @@ int main(int argc, char **argv) {
   const double length_x = 2.75;
   const double length_y = 2.0;
   const int pixel_count_x = 10;
-  Grid grid = Grid(center_x, center_y, length_x, length_y, pixel_count_x);
+  Grid* grid = new Grid(center_x, center_y, length_x, length_y, pixel_count_x);
   
   // Set up Mandelbrot object
   const double rad_max = 2.0;
   const int iter_max = 1000;
-  Mandelbrot mandelbrot = Mandelbrot(rad_max, iter_max);
+  Mandelbrot* mandelbrot = new Mandelbrot(rad_max, iter_max);
 
   // Allocate our arrays
-  Kokkos::View<double*> a("a", grid.num_pixels);
+  Kokkos::View<double*> a("a", grid->num_pixels);
 
   // Create host mirror of a
   auto  a_mirror = Kokkos::create_mirror_view(a);
 
   // parallel loop
-  MyFunctor body = MyFunctor(a);
-  Kokkos::parallel_for(grid.num_pixels, body); 
+  MyFunctor body = MyFunctor(a, grid, mandelbrot);
+  Kokkos::parallel_for(grid->num_pixels, body); 
 
   // Update the mirror
   deep_copy(a_mirror, a);
 
-  std::cout << "Done!" << endl;
+  cout << "Done!" << endl;
+
+  delete grid;
+  delete mandelbrot;
 
   Kokkos::finalize();
 
