442035938c418f3e9b24eb57531e5724ed20aa25
Ian Dunn <iansdunn@gmail.com>
2017-07-26 11:18:05 -0400

    Updated Grid interface.

--- src/Grid.h
+++ src/Grid.h
@@ -1,30 +1,43 @@
 // Grid.h
 // Created by Ian Dunn.
 
+#include <utility>
+
 class Grid
 {
 
 public:
 
-    Grid();
-    ~Grid();
+    Grid(double center_x, double center_y,
+         double length_x, double length_y, 
+         int pixel_count_x) :
+        center_x_(center_x_), center_y_(center_y),
+        length_x_(length_x), length_y_(length_y),
+        pixel_count_x_(pixel_count_x)
+    {
+        setup();
+    }
 
-    void setup();
+    std::pair<double, double> calculate_xy(int i);
+
+    int num_pixels;
 
 private:
 
+    void setup();
+
+    int pixel_count_x_;
+    int pixel_count_y_;
+    
     double center_x_;
     double center_y_;
 
     double length_x_;
     double length_y_;
 
-    int pixel_count_x_;
-    int pixel_count_y_;
-    
     double pixel_size_;
 
     double min_x_;
     double max_y_;
 
-}
+};
